4da67c827f96a239ffd557acecc4b277569332bc
Gyueon Lee <gon2yq@naver.com>
2025-08-06 14:59:55 +0900

    🎉: Initialize Project

---
+++ src/main/java/com/dasihaebom/dasihaebom/DasihaebomApplication.java
@@ -0,0 +1,13 @@
+package com.dasihaebom.dasihaebom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasihaebomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasihaebomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasihaebom/dasihaebom/DasihaebomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasihaebom.dasihaebom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasihaebomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
