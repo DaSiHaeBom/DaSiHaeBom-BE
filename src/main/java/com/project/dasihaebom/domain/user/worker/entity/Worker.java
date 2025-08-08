719282021e9eff0e479888fa8b9d8b910cd9b301
Gyueon Lee <gon2yq@naver.com>
2025-08-08 14:08:46 +0900

    ✨feat: Worker 엔티티 구현

--- src/main/java/com/project/dasihaebom/domain/user/worker/entity/Worker.java
+++ src/main/java/com/project/dasihaebom/domain/user/worker/entity/Worker.java
@@ -1,5 +1,8 @@
 package com.project.dasihaebom.domain.user.worker.entity;
 
+import com.project.dasihaebom.domain.auth.entity.WorkerAuth;
+import com.project.dasihaebom.domain.user.LoginType;
+import com.project.dasihaebom.domain.user.Role;
 import com.project.dasihaebom.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -15,4 +18,35 @@ public class Worker extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "login_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LoginType loginType;
+
+    @Column(name = "ai_summary", nullable = true)
+    private String aiSummary;
+
+    // User가 삭제되면 Auth도 삭제
+    @OneToOne(mappedBy = "worker", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private WorkerAuth workerAuth;
 }
