b0b78b30c1645eea256100a2c0b37b1dd3fe80f4
Gyueon Lee <gon2yq@naver.com>
2025-08-08 14:27:41 +0900

    ✨feat: Corp 엔티티 구현

--- src/main/java/com/project/dasihaebom/domain/user/corp/entity/Corp.java
+++ src/main/java/com/project/dasihaebom/domain/user/corp/entity/Corp.java
@@ -1,8 +1,11 @@
 package com.project.dasihaebom.domain.user.corp.entity;
 
+import com.project.dasihaebom.domain.auth.entity.CorpAuth;
+import com.project.dasihaebom.domain.user.Role;
 import com.project.dasihaebom.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.logging.log4j.util.Lazy;
 
 @Entity
 @Getter
@@ -15,4 +18,29 @@ public class Corp extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "login_id", nullable = false)
+    private String loginId;
+
+    @Column(name = "ceo_name", nullable = false)
+    private String ceoName;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "corp_number", nullable = false)
+    private String corpNumber;
+
+    @Column(name = "corp_name", nullable = false)
+    private String corpName;
+
+    @Column(name = "corp_address", nullable = false)
+    private String corpAddress;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    // User가 삭제될 때, Auth도 같이 삭제
+    @OneToOne(mappedBy = "corp", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private CorpAuth corpAuth;
 }
