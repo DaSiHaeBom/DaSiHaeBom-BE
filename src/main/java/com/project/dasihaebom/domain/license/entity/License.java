46488dba8c7643608e732d265bf1cc8512e6acac
tuna1025 <jo215098@gmail.com>
2025-08-13 21:37:49 +0900

    ✨feat: license 엔티티 작성 (맘에 안들면 수정해주세요)

--- src/main/java/com/project/dasihaebom/domain/license/entity/License.java
+++ src/main/java/com/project/dasihaebom/domain/license/entity/License.java
@@ -1,5 +1,6 @@
 package com.project.dasihaebom.domain.license.entity;
 
+import com.project.dasihaebom.domain.user.worker.entity.Worker;
 import com.project.dasihaebom.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -15,4 +16,16 @@ public class License extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
+    private String name; // 자격증명
+
+    @Column(name = "issuer")
+    private String issuer; // 발급처
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "worker_id")
+    private Worker worker;
+
 }
