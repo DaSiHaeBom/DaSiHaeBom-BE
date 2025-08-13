43a7f6e56cc586d0afad5d5b002f0c5e57d388f9
tuna1025 <jo215098@gmail.com>
2025-08-13 09:08:02 +0900

    ✨feat: 내 자기소개서 조회 기능 api 구현

--- src/main/java/com/project/dasihaebom/domain/introduction/repository/IntroductionRepository.java
+++ src/main/java/com/project/dasihaebom/domain/introduction/repository/IntroductionRepository.java
@@ -3,6 +3,10 @@ package com.project.dasihaebom.domain.introduction.repository;
 import com.project.dasihaebom.domain.introduction.entity.Introduction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IntroductionRepository extends JpaRepository<Introduction,Long> {
     void deleteAllByWorkerId(Long workerId);
+
+    Optional<Introduction> findTopByWorkerIdOrderByIdDesc(Long workerId);
 }
