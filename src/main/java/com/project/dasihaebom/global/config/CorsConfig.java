eb051f7ef6c71d7acee98998a4aad4f77786cb10
Gyueon Lee <gon2yq@naver.com>
2025-08-25 00:04:46 +0900

    💚ci: cors config 수정

--- src/main/java/com/project/dasihaebom/global/config/CorsConfig.java
+++ src/main/java/com/project/dasihaebom/global/config/CorsConfig.java
@@ -25,6 +25,8 @@ public class CorsConfig implements WebMvcConfigurer {
         allowedOrigins.add("http://localhost:5174");
         allowedOrigins.add("https://www.dlrbdjs.store");
         allowedOrigins.add("https://da-si-hae-bom-fe-tau.vercel.app");
+        allowedOrigins.add("https://api.dasihaebom.store");
+        allowedOrigins.add("https://www.dasihaebom.store");
 
         configuration.setAllowedOrigins(allowedOrigins);
 
